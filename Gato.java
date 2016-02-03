63f2bb1c97d518eec43fc712977a98b7a870d92e
Ernesto Gonzalez <negrocherne@gmail.com>
2016-02-03 13:36:08 -0600

    Clase gato

---
+++ Gato.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+
+
+public class Gato extends JFrame{
+	JPanel g=new JPanel();
+	Boton buttons[]=new Boton[9];
+
+	public static void main(String []args){
+		new Gato();
+	}
+
+}
